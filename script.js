14b7f965728f692e1c01e511637b5ae97afa09c4
StefanDodoloi <153545504+StefanDodoloi@users.noreply.github.com>
2024-01-24 13:49:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
 const words = ['web', 'game', 'class', 'player', 'hangman', 'wellcode', 'developer'];
-const randomWord = words[Math.floor(Math.random() * words.length)];
-let underscores = Array(randomWord.length).fill('_');
-let livesLeft = 7;
-document.getElementById('livesLeft').textContent = 'Lives left: ' + livesLeft;
-document.getElementById('underscores').innerHTML = underscores;
+let randomWord, underscores, livesLeft;
+
+function newGame() {
+  randomWord = words[Math.floor(Math.random() * words.length)];
+  underscores = Array(randomWord.length).fill('_');
+  livesLeft = 7;
+  document.getElementById('livesLeft').textContent = 'Lives left: ' + livesLeft;
+  document.getElementById('underscores').innerHTML = underscores;
+  document.getElementById('button').disabled = false;
+  document.getElementById('gameOver').textContent = '';
+}
 
 function checkLetter() {
   let letter = document.getElementById('letter').value;
-  let letterFound = 0;
+  let letterFound = false;
+  let message;
   document.getElementById('letter').value = ''; 
   for (let i = 0; i < randomWord.length; ++i) {
     if (randomWord[i] === letter && underscores[i] === '_') {
-      letterFound = 1;
+      letterFound = true;
       underscores[i] = randomWord[i];
       document.getElementById('underscores').innerHTML = underscores;
     }
   }
-  if (letterFound === 0) {
+  if (letterFound === false) {
     --livesLeft;
     document.getElementById('livesLeft').textContent = 'Lives left: ' + livesLeft;
   }
   if (!underscores.includes('_')) {
-    youWon();
+    message = 'You win! Correct word was: ';
+    gameOver(message);
   }
   if (livesLeft === 0) {
-    gameOver();
+    message = 'You lost! Correct word was: ';
+    gameOver(message);
   }
 }
 
-function gameOver() {
-  document.getElementById('gameOver').textContent = 'You lost! Correct word was: ' + randomWord + '.';
-  document.getElementById('button').disabled = true;
-}
-
-function youWon() {
-  document.getElementById('gameOver').textContent = 'You won!';
+function gameOver(message) {
+  document.getElementById('gameOver').textContent = message + randomWord + '.';
   document.getElementById('button').disabled = true;
 }
